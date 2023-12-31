98b20175e1aee4082946149d6db7634025cf2876
bnjmn <bmcode19@gmail.com>
2023-12-31 12:09:32 +0100

    added animation

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,42 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../../styles'
 import { logo, pic4 } from '../../assets'
 import { Brands } from '../../constants/main'
+import { motion, variants, useAnimation } from 'framer-motion'
 
 
 const About = () => {
+
+    const controls = useAnimation();
+    const controlY = useAnimation();
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        const updateAnimation = () => {
+            controls.start({
+                opacity: 1,
+                x: 0,
+                transition: { duration: 1 },
+            });
+            controlY.start({
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1 },
+            });
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // Adjust the scroll threshold as needed
+        if (scrollY > 300) {
+            updateAnimation();
+        }
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrollY, controls, controlY]);
+
+
     return (
         <div className="my-8 border-b-[1px] border-black ">
-        <section className={`flex items-end justify-between flex-col md:flex-row ${styles.paddingX} ${styles.paddingY}`}>
+            <section
 
-            <div className='flex flex-col items-start flex-[1.5]'>
-                <div className='relative'>
-                    <span className=' w-[15px] h-[15px] rounded-[100%] bg-black absolute bottom-[45%] right-[-25px]'></span>
-                    <h3 className={`${styles.heading3} py-8 uppercase`}>About</h3>
-                </div>
-                <div className={`${styles.flexCenter} py-1 md:py-4`}>
-                    <span className={`mr-2 ${styles.heading2} uppercase`}>PhotoBy</span>
-                    <img src={logo} alt="" className='w-[] md:w-[41px]' />
-                </div>
-                <img src={pic4} alt="" className='h-[228px] object-cover object-center'/>
-            </div>
-            <div className='flex-1 mt-6 md:mt-0'>
-                <p className={`text-[18px] `}>Lorem ipsum dolor sit amet consectetur. Nunc lacus imperdiet adipiscing urna. Amet aenean ac faucibus varius curabitur consequat pellentesque morbi. Tincidunt eleifend morbi mauris nascetur porta vulputate. At egestas mi senectus nam aliquam vivamus. Egestas non sit pulvinar faucibus tincidunt at quis morbi tortor. Ultrices egestas rhoncus in justo massa consectetur est ut magna. Eget vitae duis a aliquam condimentum id non scelerisque. </p>
-                <button className='py-[14px] px-[24px] md:py-[14px] md:px-[48px] bg-black text-white outline-none rounded-full my-2'>Learn More</button>
-            </div>
+                className={`flex items-end justify-between flex-col md:flex-row ${styles.paddingX} ${styles.paddingY}`}>
 
-            
-        </section>
-        <div className=' flex md:justify-between justify-around items-center flex-wrap mt-4 mb-8 '>
+                <motion.div
+                    initial={{ opacity: 0, x: -500 }}
+                    animate={controls}
+                    className='flex flex-col items-start flex-[1.5]'>
+                    <div className='relative'>
+                        <span className=' w-[15px] h-[15px] rounded-[100%] bg-black absolute bottom-[45%] right-[-25px]'></span>
+                        <h3 className={`${styles.heading3} py-8 uppercase`}>About</h3>
+                    </div>
+                    <div
+
+                        className={`${styles.flexCenter} py-1 md:py-4`}>
+                        <span className={`mr-2 ${styles.heading2} uppercase`}>PhotoBy</span>
+                        <img src={logo} alt="" className='w-[] md:w-[41px]' />
+                    </div>
+                    <img src={pic4} alt="" className='h-[228px] object-cover object-center' />
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, x: 500 }}
+                    transition={{ delay: 0.3 }}
+                    animate={controls}
+                    className='flex-1 mt-6 md:mt-0'>
+                    <p className={`text-[18px] `}>Lorem ipsum dolor sit amet consectetur. Nunc lacus imperdiet adipiscing urna. Amet aenean ac faucibus varius curabitur consequat pellentesque morbi. Tincidunt eleifend morbi mauris nascetur porta vulputate. At egestas mi senectus nam aliquam vivamus. Egestas non sit pulvinar faucibus tincidunt at quis morbi tortor. Ultrices egestas rhoncus in justo massa consectetur est ut magna. Eget vitae duis a aliquam condimentum id non scelerisque. </p>
+                    <button className='py-[14px] px-[24px] md:py-[14px] md:px-[48px] bg-black text-white outline-none rounded-full my-2'>Learn More</button>
+                </motion.div>
+
+
+            </section>
+            <div className=' flex md:justify-between justify-around items-center flex-wrap mt-4 mb-8 '>
 
                 {Brands.map((brand) => (
-                    <div key={brand.id}>
+                    <motion.div key={brand.id}
+                        initial={{ opacity: 0, }}
+                        transition={{ delay: 0.3 }}
+                        animate={controls}
+                    >
                         <img src={brand.img} alt={brand.id} />
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
     )
 }
+
 
 export default About
--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,33 +1,95 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import styles from '../../styles'
 import { pic1 } from "../../assets"
+import { motion, variants,useAnimation } from 'framer-motion'
+
+
+const textVariant = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        }
+    }
+}
 
 const Hero = () => {
+    const controls = useAnimation();
+    const [scrollY, setScrollY] = useState(0);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        setScrollY(window.scrollY);
+      };
+  
+      const updateAnimation = () => {
+        controls.start({
+          opacity: 1,
+          x: 0,
+          transition: { duration: 1 },
+        });
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      // Adjust the scroll threshold as needed
+      if (scrollY > 300) {
+        updateAnimation();
+      }
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [scrollY, controls]);
+
     return (
         <section className={`${styles.paddingX}`}>
             <div className=" my-10 flex items-center justify-between">
-                <div className='flex flex-col relative'>
-                    <div className='flex items-start'>
+                <motion.div
+                  animate={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, x: -500 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 1,  }}
+                    className='flex flex-col relative'>
+                    <motion.div
+
+                        className='flex items-start'>
                         <span className=' w-[20px] h-[20px]  sm:w-[40px] sm:h-[40px] rounded-[100%] bg-black mt-1 sm:mt-3'></span>
-                        <h1 className={`${styles.heading2} text-black`}>
+                        <motion.h1 variants={textVariant} className={`${styles.heading2} text-black`}>
                             BRAND <br />
                             RECOGNITION
-                        </h1>
-                    </div>
+                        </motion.h1>
+                    </motion.div>
                     <div className='flex items-center gap-4 mt-7'>
                         <p className={`${styles.paragraph} font-[500]`}>PHOTOGRAPHY</p>
                         <span className='w-[40px] sm:w-[183px] h-[1px] bg-black'></span>
                         <span className=' w-[20px] h-[20px] rounded-[100%] bg-black'></span>
                     </div>
-                </div>
-                <div className='relative hidden md:block'>
+                </motion.div>
+                <motion.div
+                    animate={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 500 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1, delay: 0.3 }}
+                    className='relative hidden md:block'>
                     <p className='text-[#0000000d] text-[48px] sm:text-[196px] font-[500]'>2021</p>
                     <p className=' absolute bottom-[70px] right-[40px] text-[28px] font-[500]'>Next</p>
-                </div>
+                </motion.div>
             </div>
-            <div className='w-[100%] md:min-w-[1356px] '>
+            <motion.div
+                animate={{ scale: 1, }}
+                initial={{ scale: 0, }}
+
+                transition={{ duration: 1 }}
+                className='w-[100%] md:min-w-[1356px] '>
                 <img src={pic1} alt="services" className='w-full h-[423.448px] md:h-full object-cover object-center' />
-            </div>
+            </motion.div>
         </section>
     )
 }
